1dbc16aabf3adc1894188ed5d04d826280d1cfc8
tjac1336 <38540104+tjac1336@users.noreply.github.com>
2019-09-27 13:01:05 -0500

    Added JavaScript to the app

--- scr/main.js
+++ scr/main.js
@@ -1,12 +1,31 @@
-document.querySelector('#calculate').addEventListener('click', calculate);
+document.querySelector('#calculate').addEventListener('click', shortestStringFuction);
 
-function calculate() {
-    const firstInput = parseFloat(document.querySelector('#firstInput').value);
-    const secondInput = parseFloat(document.querySelector('#secondInput').value);
-
+function shortestStringFuction() {
+    const string1 = document.querySelector('#string1');
+    const string2 = document.querySelector('#string2');
+    const string3 = document.querySelector('#string3');
     
 
-    document.querySelector('#tipAmount').innerHTML = tipAmount.toFixed(2);
-    document.querySelector('#taxTotal').innerHTML = tax.toFixed(2);
-    document.querySelector('#grandTotal').innerHTML = (total + tipAmount + tax).toFixed(2);
+    let str1length = (string1.value).length;
+    let str2length = (string2.value).length;
+    let str3length = (string3.value).length;
+
+
+    if (str1length < str2length && str1length < str3length)
+    {
+        document.querySelector('#shortestStringLbl').innerHTML = string1.value;
+    }
+    if (str2length < str1length && str2length < str3length)
+    {
+        document.querySelector('#shortestStringLbl').innerHTML = string2.value;
+    }
+    if (str3length < str1length && str3length < str2length)
+    {
+        document.querySelector('#shortestStringLbl').innerHTML = string3.value;
+    }
+    if (str1length == str2length || str1length == str3length || str2length == str3length)
+    {
+        document.querySelector('#shortestStringLbl').innerHTML = "Two or more are equal"
+    }
+
 };
